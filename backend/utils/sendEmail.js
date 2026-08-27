8137c5b7ec22aff2806ac18be84f63854faaf13f
Meet Patel <meetpatel.dev45@gmail.com>
2026-08-27 11:04:08 +0530

    Improve parameter structure in sendEmail function
    Refactor sendEmail function parameters for clarity.

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,22 +4,28 @@ const transport = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-const sendEmail = async ({ email, subject, body }) => {
-  return transport.sendMail({
+const sendEmail = async ({
+  email,
+  subject,
+  body,
+}) => {
+  const info = await transport.sendMail({
     from: `"Auth App" <${process.env.EMAIL_USER}>`,
     to: email,
     subject,
     text: body,
   });
+
+  return info;
 };
 
 module.exports = {
-  transport,
   sendEmail,
 };
